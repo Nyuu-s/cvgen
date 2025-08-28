1d6d02ba88d561ba8950af5c505353c6af0f92c3
Edgar Jacquemoud <cookiesarenightmares@gmail.com>
2025-08-28 21:57:52 +0200

    boilerplate output generation

--- htlmgen.c
+++ htlmgen.c
@@ -18,7 +18,14 @@ typedef struct IOFile {
     size_t size;
     size_t loc;
     FILE* fd;
+    char eof;
 } IOFile;
+
+typedef struct DocElmProperties
+{
+    unsigned int font_size;
+} DocElmProperties;
+
 
 typedef struct StringBuilder {
     char* data;
@@ -49,13 +56,20 @@ int get_next_token(IOFile* file, StringBuilder* sb){
     while (file->loc < file->size && !(file->loc+1 < file->size && file->content[file->loc] == '#' && file->content[file->loc+1] == '[')){
         file->loc++;
     }
-    if(file->loc >= file->size) return 0;
+    if(file->loc >= file->size)
+    {
+        file->eof = 1;
+        return 0;
+    }
 
     while(file->loc < file->size && file->content[file->loc] != ']'){
         sb_add(sb, file->content[file->loc])
         file->loc++;
     }
-    if(file->loc >= file->size) return 0;
+    if(file->loc >= file->size)  {
+        file->eof = 1;
+        return 0;
+    };
     sb_add(sb, file->content[file->loc++]);
     sb_add(sb, '\0');
     printf("token: %s\n", sb->data);
@@ -75,7 +89,7 @@ int main() {
     }
     input.size = get_file_size(input.fd);
     input.content = (char*) malloc(sizeof(char) * input.size);
-    if(fread(input.content, input.size, sizeof(char), input.fd) <= 0){
+    if(fread(input.content, sizeof(char), input.size,input.fd) <= 0){
         printf("Error on reading input file!");
         return 0;
     }
@@ -88,13 +102,38 @@ int main() {
         return 1;
     }
 
+    //gen boilerplate
+    fwrite("<!DOCTYPE html>\n",sizeof(char), 16, output.fd);
+    fwrite("<html lang=\"en\">\n",sizeof(char), 17, output.fd);
+    fwrite("<head>\n",sizeof(char), 7, output.fd);
+    fwrite("<meta charset=\"UTF-8\">\n",sizeof(char), 23, output.fd);
+    fwrite("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n",sizeof(char), 71, output.fd);
+    
+    
+    DocElmProperties default_prop = {
+        .font_size = 12
+    };
+    
     StringBuilder sb = {0};
+    char title_found = 0;
     while(get_next_token(&input, &sb)){
-        // do stuff with token value in sb
-        if(strcmp(sb.data, document_elements[ELM_TITLE]) == 0){
-            printf("FOUND TITLE");
+        if(strcmp(sb.data, document_elements[ELM_TITLE]) == 0) {
+            title_found = 1;
             break;
         }
+        sb.count = 0;
+    }
+    if(input.eof || !title_found){
+        printf("Expected a title but coundl't find one!");
+        return 1;
     }
 
+    //gen title
+    fwrite("<title>",sizeof(char), 7, output.fd);
+    fwrite(sb.data,sizeof(char), sb.count-1, output.fd);
+    //gen boilerplate
+    fwrite("</title>\n",sizeof(char), 9, output.fd);
+    fwrite("</head>\n<body>\n",sizeof(char), 15, output.fd);
+    fwrite("</body>\n</html>",sizeof(char), 15, output.fd);
+    
 }
